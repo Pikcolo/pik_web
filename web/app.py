09a0c8c992e09d42b88986e4ed0af58bf9c6e673
Sirawich Noipha <pikcolo13@gmail.com>
2024-02-06 23:00:24 +0700

    fix app.py

--- web/app.py
+++ web/app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
-@app.route('/')
-def index():
-    return render_template('index.html')
 
+@app.route("/")
+def home():
+    return render_template("index.html", content="Testing")
+
+if __name__ == "__main__":
+    app.run(debug=True)
