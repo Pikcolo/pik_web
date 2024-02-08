f2e138ec0b4a526b4def818b38fcbbe0df3c7d60
Sirawich Noipha <pikcolo13@gmail.com>
2024-02-08 16:39:38 +0700

    fix app.py

--- web/app.py
+++ web/app.py
@@ -3,12 +3,13 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def Home():
     return render_template("index.html")
 
 @app.route("/About")
 def About():
-    return render_template("About.html")
+    services =['ตัดหญ้า', 'ทำความสะอาดบ้าน', 'รับจ้างเล่นเกมแทน' ,'สอนการบ้าน', 'ขับรถรับ-ส่ง']
+    return render_template("About.html", my_services = services )
 
 if __name__ == "__main__":
     app.run(debug=True)
